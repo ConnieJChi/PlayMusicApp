51f88eaf6822ae0665d9dd58cf9ed10845876228
Connie Chi <conniejchi@berkeley.edu>
2018-06-15 21:15:58 -0700

    Updated to show Toast text to tell when song is playing, paused, and finished.

--- app/src/main/java/com/example/android/playmusicapp/MainActivity.java
+++ app/src/main/java/com/example/android/playmusicapp/MainActivity.java
@@ -4,6 +4,7 @@ import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     MediaPlayer m;
@@ -12,15 +13,25 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         m = MediaPlayer.create(this, R.raw.crazy);
+
+//        int i = R.raw.crazy;
     }
 
 
 
     public void onClickStart(View v) {
+        Toast.makeText(MainActivity.this, "Playing!", Toast.LENGTH_SHORT).show();
         m.start();
+        m.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                Toast.makeText(MainActivity.this, "Finished Playing!", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     public void onClickPause(View v) {
+        Toast.makeText(MainActivity.this, "Paused", Toast.LENGTH_SHORT).show();
         m.pause();
     }
 }
